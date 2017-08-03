cb7a5b0939f8afa8fe5d6298c99db5532c6711ac
likun <496546144@qq.com>
2017-08-03 23:30:08 +0800

    statusbar 优化

---
+++ app/src/main/java/utils/hbung/com/utils/StatusActivity.java
@@ -0,0 +1,26 @@
+package utils.hbung.com.utils;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.hbung.utils.Utils;
+import com.hbung.utils.ui.StatusBarCompat;
+
+/**
+ * 作者　　: 李坤
+ * 创建时间:2017/8/3 0003　22:05
+ * 邮箱　　：496546144@qq.com
+ * <p>
+ * 功能介绍：
+ */
+
+public class StatusActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Utils.init(getApplication(), false);
+
+        setContentView(R.layout.activity_main);
+        new StatusBarCompat(this).setColorBar(R.color.red);
+    }
+}
