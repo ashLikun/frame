ec7cac1c745033a8e25511169292c294cf3357cd
likun <likun01@vcredit.com>
2017-09-07 16:24:28 +0800

    优化

--- app/src/main/java/utils/hbung/com/utils/MainActivity.java
+++ app/src/main/java/utils/hbung/com/utils/MainActivity.java
@@ -2,11 +2,9 @@ package utils.hbung.com.utils;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 
 import com.ashlikun.xrecycleview.OnLoaddingListener;
 import com.ashlikun.xrecycleview.RefreshLayout;
-import com.hbung.supertoobar.SupperToolBar;
 
 public class MainActivity extends AppCompatActivity implements RefreshLayout.OnRefreshListener, OnLoaddingListener {
 
@@ -14,15 +12,15 @@ public class MainActivity extends AppCompatActivity implements RefreshLayout.OnR
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_main);
-        SupperToolBar toolBar = (SupperToolBar) findViewById(R.id.toolBar);
-        toolBar.setBack(this);
-        toolBar.setTitle("aaaa");
-        findViewById(R.id.actionButton).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-            }
-        });
+        setContentView(R.layout.base_load_retry);
+//        SupperToolBar toolBar = (SupperToolBar) findViewById(R.id.toolBar);
+//        toolBar.setBack(this);
+//        toolBar.setTitle("aaaa");
+//        findViewById(R.id.actionButton).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//            }
+//        });
 
     }
 
