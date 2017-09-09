17014ed8d6c4bd9c0d9794c68a350e191f35c50d
likun <496546144@qq.com>
2017-09-09 21:05:37 +0800

    优化

--- http/src/main/java/com/hbung/http/db/LiteOrmUtil.java
+++ http/src/main/java/com/hbung/http/db/LiteOrmUtil.java
@@ -3,7 +3,6 @@ package com.hbung.http.db;
 
 import android.app.Application;
 
-import com.hbung.http.R;
 import com.litesuits.orm.LiteOrm;
 import com.litesuits.orm.db.DataBaseConfig;
 
@@ -15,9 +14,9 @@ public class LiteOrmUtil {
     private LiteOrmUtil() {
     }
 
-    public static void init(Application application,int versionCode) {
+    public static void init(Application application, int versionCode, String dbName) {
         if (liteOrm == null) {
-            DataBaseConfig config = new DataBaseConfig(application, application.getString(R.string.app_name));
+            DataBaseConfig config = new DataBaseConfig(application, dbName);
             config.debugged = true; // open the log
             config.dbVersion = versionCode; // set database version
             config.onUpdateListener = null; // set database update listener
