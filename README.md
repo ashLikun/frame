c496b559db5232fb87e2d42b4d31905b34ac60d7
likun <496546144@qq.com>
2018-01-01 22:59:29 +0800

    aaa

--- README.md
+++ README.md
@@ -38,5 +38,16 @@ dependencies {
     	compile 'com.github.ashLikun.frame:glideutils:{latest version}'//glide封装
 }
 ```
+#Glide库的混淆
+-keep public class * extends com.bumptech.glide.module.AppGlideModule
+-keep class com.bumptech.glide.GeneratedAppGlideModuleImpl
+-keep public enum com.bumptech.glide.load.resource.bitmap.ImageHeaderParser$** {
+    **[] $VALUES;
+    public *;
+}
 
+#OkHttp库的混淆
+-dontwarn okio.**
+-dontwarn javax.annotation.Nullable
+-dontwarn javax.annotation.ParametersAreNonnullByDefault
 
--- glideutils/src/main/java/com/ashlikun/glideutils/OkHttpGlideModule.java
+++ glideutils/src/main/java/com/ashlikun/glideutils/OkHttpGlideModule.java
@@ -7,7 +7,7 @@ import com.bumptech.glide.Registry;
 import com.bumptech.glide.annotation.GlideModule;
 import com.bumptech.glide.integration.okhttp3.OkHttpUrlLoader;
 import com.bumptech.glide.load.model.GlideUrl;
-import com.bumptech.glide.module.AppGlideModule;
+import com.bumptech.glide.module.LibraryGlideModule;
 
 import java.io.InputStream;
 
@@ -19,7 +19,7 @@ import java.io.InputStream;
  * 功能介绍：
  */
 @GlideModule
-public class OkHttpGlideModule extends AppGlideModule {
+public class OkHttpGlideModule extends LibraryGlideModule {
     @Override
     public void registerComponents(Context context, Glide glide, Registry registry) {
         registry.replace(GlideUrl.class, InputStream.class, new OkHttpUrlLoader.Factory());
