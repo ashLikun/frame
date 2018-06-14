b2839436450ce73c7355a85b3f1040f2e781e779
likun <496546144@qq.com>
2018-06-14 15:31:01 +0800

    优化

--- flatbutton/src/main/java/com/ashlikun/flatbutton/FlatButton.java
+++ flatbutton/src/main/java/com/ashlikun/flatbutton/FlatButton.java
@@ -30,21 +30,30 @@ public class FlatButton extends TextView {
     //边框颜色
     private int strokeColor = Color.TRANSPARENT;
 
-    //按下颜色
+    //按下颜色 2,3是渐变,默认没有
     private int colorPressed;
+    private int colorPressed2;
+    private int colorPressed3;
     private int colorPressedText;
-    //默认的颜色
+    //默认的颜色 2,3是渐变,默认没有
     private int colorNormal;
+    private int colorNormal2;
+    private int colorNormal3;
     private int textColor;
-    //不可用的颜色
+    //不可用的颜色 2,3是渐变,默认没有
     private int colorEnable;
+    private int colorEnable2;
+    private int colorEnable3;
     private int colorEnableText;
     //水波纹颜色
     private int colorRipple;
     private int clickDelay = 200;
     //textColor是否使用ColorStateList
     private boolean isUseTextColorList = true;
-
+    //渐变的方向
+    private int colorNormalOrientation = -1;
+    private int colorPressedOrientation = -1;
+    private int colorEnableOrientation = -1;
 
     private long lastClickTime = 0;
 
@@ -71,15 +80,25 @@ public class FlatButton extends TextView {
         strokeColor = attr.getColor(R.styleable.FlatButton_strokeColor, strokeColor);
         strokeWidth = attr.getDimension(R.styleable.FlatButton_strokeWidth, strokeWidth);
         colorPressed = attr.getColor(R.styleable.FlatButton_colorPressed, 0xffeeeeee);
+        colorPressed2 = attr.getColor(R.styleable.FlatButton_colorPressed2, 0);
+        colorPressed3 = attr.getColor(R.styleable.FlatButton_colorPressed3, 0);
         colorNormal = attr.getColor(R.styleable.FlatButton_colorNormal, Color.TRANSPARENT);
+        colorNormal2 = attr.getColor(R.styleable.FlatButton_colorNormal2, 0);
+        colorNormal3 = attr.getColor(R.styleable.FlatButton_colorNormal3, 0);
         textColor = attr.getColor(R.styleable.FlatButton_android_textColor, Color.GRAY);
         colorPressedText = attr.getColor(R.styleable.FlatButton_colorPressedText, getCurrentTextColor());
         colorRipple = attr.getColor(R.styleable.FlatButton_colorRipple, colorPressed);
         colorEnable = attr.getColor(R.styleable.FlatButton_colorEnable, colorPressed);
+        colorEnable2 = attr.getColor(R.styleable.FlatButton_colorEnable2, 0);
+        colorEnable3 = attr.getColor(R.styleable.FlatButton_colorEnable3, 0);
         colorEnableText = attr.getColor(R.styleable.FlatButton_colorEnableText, colorPressedText);
         clickDelay = attr.getInt(R.styleable.FlatButton_clickDelay, clickDelay);
+        colorNormalOrientation = attr.getInt(R.styleable.FlatButton_colorNormalOrientation, -1);
+        colorPressedOrientation = attr.getInt(R.styleable.FlatButton_colorPressedOrientation, -1);
+        colorEnableOrientation = attr.getInt(R.styleable.FlatButton_colorEnableOrientation, -1);
         isUseTextColorList = attr.getBoolean(R.styleable.FlatButton_isUseTextColorList, isUseTextColorList);
-        setClickable(true);//默认可以点击
+        //默认可以点击
+        setClickable(true);
         if (!isUseBackground) {
             setBackgroundCompat(getStateListDrawable());
         }
@@ -116,15 +135,15 @@ public class FlatButton extends TextView {
     private Drawable createNormalDrawable() {
         GradientDrawable drawableBottom = new GradientDrawable();
         drawableBottom.setCornerRadius(getCornerRadius());
-        drawableBottom.setColor(colorNormal);
         drawableBottom.setStroke((int) strokeWidth, strokeColor);
+        setGradientDrawableColor(drawableBottom, colorNormalOrientation, colorNormal, colorNormal2, colorNormal3);
         return drawableBottom;
     }
 
     private Drawable createPressedDrawable() {
         GradientDrawable drawablePressed = new GradientDrawable();
         drawablePressed.setCornerRadius(getCornerRadius());
-        drawablePressed.setColor(colorPressed);
+        setGradientDrawableColor(drawablePressed, colorPressedOrientation, colorPressed, colorPressed2, colorPressed3);
         drawablePressed.setStroke((int) strokeWidth, strokeColor);
         return drawablePressed;
     }
@@ -132,7 +151,7 @@ public class FlatButton extends TextView {
     private Drawable createEnableDrawable() {
         GradientDrawable drawablePressed = new GradientDrawable();
         drawablePressed.setCornerRadius(getCornerRadius());
-        drawablePressed.setColor(colorEnable);
+        setGradientDrawableColor(drawablePressed, colorEnableOrientation, colorEnable, colorEnable2, colorEnable3);
         drawablePressed.setStroke((int) strokeWidth, strokeColor);
         return drawablePressed;
     }
@@ -143,6 +162,52 @@ public class FlatButton extends TextView {
         drawablePressed.setColor(colorRipple);
         drawablePressed.setStroke((int) strokeWidth, strokeColor);
         return drawablePressed;
+    }
+
+    private void setGradientDrawableColor(GradientDrawable drawable, int colorOrientation, int... color) {
+        GradientDrawable.Orientation orientation = getGradientOrientation(colorOrientation);
+        if (orientation != null) {
+            drawable.setOrientation(orientation);
+            int size = 0;
+            for (int a : color) {
+                if (a != 0) {
+                    size++;
+                }
+            }
+            int[] colorRes = new int[size];
+            int aI = 0;
+            for (int a : color) {
+                if (a != 0) {
+                    colorRes[aI++] = a;
+                }
+            }
+            drawable.setColors(colorRes);
+        } else {
+            drawable.setColor(color[0]);
+        }
+    }
+
+    private GradientDrawable.Orientation getGradientOrientation(int colorOrientation) {
+        switch (colorOrientation) {
+            case 0:
+                return GradientDrawable.Orientation.TOP_BOTTOM;
+            case 1:
+                return GradientDrawable.Orientation.TR_BL;
+            case 2:
+                return GradientDrawable.Orientation.RIGHT_LEFT;
+            case 3:
+                return GradientDrawable.Orientation.BR_TL;
+            case 4:
+                return GradientDrawable.Orientation.BOTTOM_TOP;
+            case 5:
+                return GradientDrawable.Orientation.BL_TR;
+            case 6:
+                return GradientDrawable.Orientation.LEFT_RIGHT;
+            case 7:
+                return GradientDrawable.Orientation.TL_BR;
+            default:
+                return null;
+        }
     }
 
     public Drawable getDrawable(int id) {
@@ -176,8 +241,30 @@ public class FlatButton extends TextView {
 
     //背景  设置默认的颜色
     public void setColorNormal(@ColorRes int colorNormal) {
-        if (colorNormal <= 0) return;
+        if (colorNormal == 0) {
+            return;
+        }
         this.colorNormal = getColor(colorNormal);
+        if (!isUseBackground) {
+            setBackgroundCompat(getStateListDrawable());
+        }
+    }
+
+    public void setColorNormal2(@ColorRes int colorNormal) {
+        if (colorNormal == 0) {
+            return;
+        }
+        this.colorNormal2 = getColor(colorNormal);
+        if (!isUseBackground) {
+            setBackgroundCompat(getStateListDrawable());
+        }
+    }
+
+    public void setColorNormal3(@ColorRes int colorNormal) {
+        if (colorNormal == 0) {
+            return;
+        }
+        this.colorNormal3 = getColor(colorNormal);
         if (!isUseBackground) {
             setBackgroundCompat(getStateListDrawable());
         }
@@ -185,8 +272,30 @@ public class FlatButton extends TextView {
 
     //背景   设置按下的颜色
     public void setColorPressed(@ColorRes int colorPressed) {
-        if (colorPressed <= 0) return;
+        if (colorPressed == 0) {
+            return;
+        }
         this.colorPressed = getColor(colorPressed);
+        if (!isUseBackground) {
+            setBackgroundCompat(getStateListDrawable());
+        }
+    }
+
+    public void setColorPressed2(@ColorRes int colorPressed) {
+        if (colorPressed == 0) {
+            return;
+        }
+        this.colorPressed2 = getColor(colorPressed);
+        if (!isUseBackground) {
+            setBackgroundCompat(getStateListDrawable());
+        }
+    }
+
+    public void setColorPressed3(@ColorRes int colorPressed) {
+        if (colorPressed == 0) {
+            return;
+        }
+        this.colorPressed3 = getColor(colorPressed);
         if (!isUseBackground) {
             setBackgroundCompat(getStateListDrawable());
         }
@@ -194,8 +303,66 @@ public class FlatButton extends TextView {
 
     //背景，设置不可用的颜色
     public void setColorEnable(@ColorRes int colorEnable) {
-        if (colorEnable <= 0) return;
+        if (colorEnable == 0) {
+            return;
+        }
         this.colorEnable = getColor(colorEnable);
+        if (!isUseBackground) {
+            setBackgroundCompat(getStateListDrawable());
+        }
+    }
+
+    public void setColorEnable2(@ColorRes int colorEnable) {
+        if (colorEnable == 0) {
+            return;
+        }
+        this.colorEnable2 = getColor(colorEnable);
+        if (!isUseBackground) {
+            setBackgroundCompat(getStateListDrawable());
+        }
+    }
+
+    public void setColorEnable3(@ColorRes int colorEnable) {
+        if (colorEnable == 0) {
+            return;
+        }
+        this.colorEnable3 = getColor(colorEnable);
+        if (!isUseBackground) {
+            setBackgroundCompat(getStateListDrawable());
+        }
+    }
+
+    /**
+     * 设置渐变方向
+     *
+     * @param colorOrientation
+     */
+    public void setColorNormalOrientation(int colorOrientation) {
+        this.colorNormalOrientation = colorOrientation;
+        if (!isUseBackground) {
+            setBackgroundCompat(getStateListDrawable());
+        }
+    }
+
+    /**
+     * 设置渐变方向
+     *
+     * @param colorOrientation
+     */
+    public void setColorPressedOrientation(int colorOrientation) {
+        this.colorPressedOrientation = colorOrientation;
+        if (!isUseBackground) {
+            setBackgroundCompat(getStateListDrawable());
+        }
+    }
+
+    /**
+     * 设置渐变方向
+     *
+     * @param colorOrientation
+     */
+    public void setColorEnableOrientation(int colorOrientation) {
+        this.colorEnableOrientation = colorOrientation;
         if (!isUseBackground) {
             setBackgroundCompat(getStateListDrawable());
         }
@@ -203,7 +370,9 @@ public class FlatButton extends TextView {
 
     //设置水波纹颜色
     public void setColorRipple(@ColorRes int colorRipple) {
-        if (colorRipple <= 0) return;
+        if (colorRipple == 0) {
+            return;
+        }
         this.colorRipple = getColor(colorRipple);
         if (!isUseBackground) {
             setBackgroundCompat(getStateListDrawable());
@@ -212,7 +381,9 @@ public class FlatButton extends TextView {
 
     //设置圆角
     public void setCornerRadius(@DimenRes int cornerRadius) {
-        if (cornerRadius <= 0) return;
+        if (cornerRadius == 0) {
+            return;
+        }
         this.cornerRadius = getDimension(cornerRadius);
         if (!isUseBackground) {
             setBackgroundCompat(getStateListDrawable());
@@ -221,7 +392,9 @@ public class FlatButton extends TextView {
 
     //设置边框颜色
     public void setStrokeColor(@ColorRes int strokeColor) {
-        if (strokeColor <= 0) return;
+        if (strokeColor == 0) {
+            return;
+        }
         this.strokeColor = getColor(strokeColor);
         if (!isUseBackground) {
             setBackgroundCompat(getStateListDrawable());
@@ -230,42 +403,53 @@ public class FlatButton extends TextView {
 
     //设置边框大小
     public void setStrokeWidth(@DimenRes int strokeWidth) {
-        if (strokeWidth <= 0) return;
+        if (strokeWidth == 0) {
+            return;
+        }
         this.strokeWidth = getDimension(strokeWidth);
         if (!isUseBackground) {
             setBackgroundCompat(getStateListDrawable());
         }
     }
+
     //默认文字颜色
-
-
     @Override
     public void setTextColor(@ColorRes int textColor) {
-        if (textColor <= 0) return;
+        if (textColor == 0) {
+            return;
+        }
         this.textColor = getColor(textColor);
         super.setTextColor(this.textColor);
     }
 
     //设置文字按下的颜色
     public void setColorPressedText(@ColorRes int colorPressedText) {
-        if (colorPressedText <= 0) return;
+        if (colorPressedText == 0) {
+            return;
+        }
         this.colorPressedText = getColor(colorPressedText);
-        if (isUseTextColorList)
+        if (isUseTextColorList) {
             setTextColor(getColorStateList());
+        }
     }
 
     //设置按钮不可用 时候  文字颜色
     public void setColorEnableText(@ColorRes int colorEnableText) {
-        if (colorEnableText <= 0) return;
+        if (colorEnableText == 0) {
+            return;
+        }
         this.colorEnableText = getColor(colorEnableText);
-        if (isUseTextColorList)
+        if (isUseTextColorList) {
             setTextColor(getColorStateList());
+        }
     }
+
 
     public void setUseTextColorList(boolean useTextColorList) {
         isUseTextColorList = useTextColorList;
-        if (isUseTextColorList)
+        if (isUseTextColorList) {
             setTextColor(getColorStateList());
+        }
     }
 
     //设置是否使用外部设置的background
