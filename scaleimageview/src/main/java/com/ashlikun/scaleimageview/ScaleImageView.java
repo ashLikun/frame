585f59656fabe91ec512ec825115c1759f0efba6
likun <496546144@qq.com>
2018-08-03 17:44:17 +0800

    提取出Banner

--- scaleimageview/src/main/java/com/ashlikun/scaleimageview/ScaleImageView.java
+++ scaleimageview/src/main/java/com/ashlikun/scaleimageview/ScaleImageView.java
@@ -7,28 +7,16 @@ import android.widget.ImageView;
 
 
 public class ScaleImageView extends ImageView {
-    private int widthScale = 1;
-    private int heightScale = 1;
-    private int primary = 1;
+    /**
+     * 大小比例，按照宽度
+     */
+    private float ratio = 16 / 9.0f;
 
     public ScaleImageView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
-        TypedArray a = context.obtainStyledAttributes(attrs,
-                R.styleable.ScaleImageView);
-        int n = a.getIndexCount();
-        for (int i = 0; i < n; i++) {
-            int attr = a.getIndex(i);
-            if (attr == R.styleable.ScaleImageView_widthScale) {
-                widthScale = a.getInt(attr, 1);
-            } else if (attr == R.styleable.ScaleImageView_heightScale) {
-                heightScale = a.getInt(attr, 1);
-            } else if (attr == R.styleable.ScaleImageView_primary) {
-                primary = a.getInt(R.styleable.ScaleImageView_primary, 1);
-            }
-        }
-
-
-        a.recycle();
+        TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.ScaleImageView);
+        ratio = ta.getFloat(R.styleable.ScaleImageView_sci_ratio, ratio);
+        ta.recycle();
     }
 
     public ScaleImageView(Context context, AttributeSet attrs) {
@@ -41,51 +29,21 @@ public class ScaleImageView extends ImageView {
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        int width = widthMeasureSpec;
-        int height = heightMeasureSpec;
-        if (primary == 1) {
-            width = MeasureSpec.getSize(widthMeasureSpec);
-            height = (int) Math.ceil((float) width * heightScale
-                    / (float) widthScale);
-        } else if (primary == 2) {
-            height = MeasureSpec.getSize(heightMeasureSpec);
-            width = (int) Math.ceil((float) height * widthScale
-                    / (float) heightScale);
+        if (ratio <= 0) {
+            super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+            return;
         }
-        setMeasuredDimension(width, height);
+        int widthSize = MeasureSpec.getSize(widthMeasureSpec);
+        int height = (int) (widthSize / ratio);
+        super.onMeasure(MeasureSpec.makeMeasureSpec(widthSize, MeasureSpec.EXACTLY), MeasureSpec.makeMeasureSpec(height, MeasureSpec.EXACTLY));
     }
 
-    @Override
-    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
-        super.onLayout(changed, left, top, right, bottom);
-
+    public float getRatio() {
+        return ratio;
     }
 
-    public int getWidthScale() {
-        return widthScale;
+    public void setRatio(float ratio) {
+        this.ratio = ratio;
+        requestLayout();
     }
-
-    public void setWidthScale(int widthScale) {
-        this.widthScale = widthScale;
-        invalidate();
-    }
-
-    public int getHeightScale() {
-        return heightScale;
-    }
-
-    public void setHeightScale(int heightScale) {
-        this.heightScale = heightScale;
-        invalidate();
-    }
-
-    public int getPrimary() {
-        return primary;
-    }
-
-    public void setPrimary(int primary) {
-        this.primary = primary;
-        invalidate();
-    }
-
 }
